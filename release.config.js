beaa6b466df03830da914df52bae86c9765eb4fc
Shane Froebel <sfroebel@tbh.org>
2023-12-18 09:00:18 -0500

    feat: make this npm public x2

--- release.config.js
+++ release.config.js
@@ -1,15 +1,5 @@
 const releaseConfig = require('./index.js')
 
 module.exports = {
-    ...releaseConfig,
-    "verifyConditions": [
-        '@semantic-release/changelog',
-       [
-           '@semantic-release/npm',
-           {
-               "access": "public"
-           }
-       ],
-        '@semantic-release/git'
-    ]
+    ...releaseConfig
 }
